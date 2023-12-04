cd83526cd4793ce2a29c8f03b0457033ece404a9
magdalenabaric <baric.magdalena2@gmail.com>
2023-12-05 00:15:02 +0100

    UntitledTestCase

---
+++ RUAP_LV4/RUAP_LV4/UnitTest1.cs
@@ -0,0 +1,139 @@
+﻿using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using System.Text;
+
+
+namespace RUAP_LV4
+{
+    [TestFixture]
+    public class UntitledTestCase
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheUntitledTestCaseTest()
+        {
+            //dopuna
+            Random random = new Random();
+            string RandomName = "User" + random.Next(1000, 9999);
+            Console.WriteLine($"Nasumično generirano ime: {RandomName}");
+
+
+            driver.Navigate().GoToUrl("https://demowebshop.tricentis.com/");
+            driver.FindElement(By.LinkText("Register")).Click();
+            driver.FindElement(By.Id("gender-female")).Click();
+            driver.FindElement(By.Id("FirstName")).Click();
+            driver.FindElement(By.Id("FirstName")).Clear();
+            driver.FindElement(By.Id("FirstName")).SendKeys("magdalena");
+            driver.FindElement(By.Id("LastName")).Click();
+            driver.FindElement(By.Id("LastName")).Clear();
+            driver.FindElement(By.Id("LastName")).SendKeys("baric");
+            driver.FindElement(By.Id("Email")).Click();
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("baric.magdalena2@gmail.com");
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("99YrcCpP2509");
+            driver.FindElement(By.Id("ConfirmPassword")).Click();
+            driver.FindElement(By.Id("ConfirmPassword")).Clear();
+            driver.FindElement(By.Id("ConfirmPassword")).SendKeys("99YrcCpP2509");
+            driver.FindElement(By.Id("register-button")).Click();
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("Email")).Click();
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("baric.magdalena2@gmail.com");
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("99YrcCpP2509");
+            driver.FindElement(By.XPath("//input[@value='Log in']")).Click();
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("99YrcCpP");
+            driver.FindElement(By.XPath("//input[@value='Log in']")).Click();
+            driver.FindElement(By.XPath("//div[3]/div/div[2]/div[3]/div[2]/input")).Click();
+            driver.FindElement(By.Id("small-searchterms")).Click();
+            driver.FindElement(By.Id("small-searchterms")).Clear();
+            driver.FindElement(By.Id("small-searchterms")).SendKeys("laptop");
+            driver.FindElement(By.XPath("//input[@value='Search']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='Tricentis Demo Web Shop']")).Click();
+            driver.FindElement(By.XPath("//div[@id='newsletter-subscribe-block']/span")).Click();
+            driver.FindElement(By.Id("newsletter-email")).Click();
+            driver.FindElement(By.Id("newsletter-email")).Clear();
+            driver.FindElement(By.Id("newsletter-email")).SendKeys("baric.magdalena2@gmail.com");
+            driver.FindElement(By.Id("newsletter-subscribe-button")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
